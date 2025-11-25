161becca3658644a9284cbb799123e8eac845001
Yoossef Balboli <yoossefbalboli@gmail.com.com>
2025-11-24 20:19:14 -0500

    Add project files.

---
+++ DeanLink/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeanLink.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
